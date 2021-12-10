d39a06204b3043deed373756a1a902bae91da378
Akmal Abdul Rahman <cowdinosaur@gmail.com>
2021-12-10 10:31:36 +0800

    rearranged the code blocks exposed

--- morsecode.ts
+++ morsecode.ts
@@ -1,148 +1,322 @@
-/**
- * Provides access to a morse code abstraction.
- */
-//% color=#000080 icon="\uf086" block="Morse Code"
-namespace morseCode {
-  export enum MorseSignal {
+enum MorseCharacter {
+    //% block="A"
+    A,
+    //% block="B"
+    B,
+    //% block="C"
+    C,
+    //% block="D"
+    D,
+    //% block="E"
+    E,
+    //% block="F"
+    F,
+    //% block="G"
+    G,
+    //% block="H"
+    H,
+    //% block="I"
+    I,
+    //% block="J"
+    J,
+    //% block="K"
+    K,
+    //% block="L"
+    L,
+    //% block="M"
+    M,
+    //% block="N"
+    N,
+    //% block="O"
+    O,
+    //% block="P"
+    P,
+    //% block="Q"
+    Q,
+    //% block="R"
+    R,
+    //% block="S"
+    S,
+    //% block="T"
+    T,
+    //% block="U"
+    U,
+    //% block="V"
+    V,
+    //% block="W"
+    W,
+    //% block="X"
+    X,
+    //% block="Y"
+    Y,
+    //% block="Z"
+    Z
+}
+enum MorseSignal {
     //% block="•"
     Dot,
     //% block="━"
     Dash,
-  }
-
-  //% fixedInstances
-  export class MorseCharacter {
-    protected _signals: MorseSignal[];
-
-    constructor(signals: MorseSignal[]) {
-      this._signals = signals;
-    }
-
-    /**
-     * Set a character's signal array.
-     * @param signals new signal array; eg: [MorseSignal.Dot]
-     */
-    //% color=#1e90ff block="set character %character to %signals"
-    public set(signals: MorseSignal[]): void {
-      this._signals = signals;
-    }
-
-    /**
-     * Get a character's signal array.
-     */
-    //% color=#1e90ff block="character %c"
-    public get(): MorseSignal[] {
-      return this._signals;
-    }
-  }
-
-  /**
-   * Represents a signal block
-   */
-  //% color=#7851a9 block="%key"
-  export function _signal(signal: MorseSignal): MorseSignal {
-    return signal;
-  }
-
-  let _words: { [k: string]: MorseSignal[][] } = {};
-
-  /**
-   * Creates a new word with a character array.
-   * @param wordName the name of the word; eg: "morse"
-   * @param characters new character array; eg: [MorseCharacter.A]
-   */
-  //% color=#006400 block="set word %wordName to %signals"
-  export function setWord(wordName: string, characters: MorseSignal[][]): void {
-    _words[wordName] = characters;
-  }
-
-  /**
-   * Get a word's character array.
-   */
-  //% color=#006400 block="word %wordName"
-  export function getWord(wordName: string): MorseSignal[][] {
-    return _words[wordName];
-  }
-
-  const _characters: { [k: string]: MorseSignal[] } = {
-    A: [MorseSignal.Dot, MorseSignal.Dash],
-    B: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dot],
-    C: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dot],
-    D: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dot],
-    E: [MorseSignal.Dot],
-    F: [MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dot],
-    G: [MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dot],
-    H: [MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dot],
-    I: [MorseSignal.Dot, MorseSignal.Dot],
-    J: [MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dash],
-    K: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dash],
-    L: [MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dot],
-    M: [MorseSignal.Dash, MorseSignal.Dash],
-    N: [MorseSignal.Dash, MorseSignal.Dot],
-    O: [MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dash],
-    P: [MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dot],
-    Q: [MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dash],
-    R: [MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dot],
-    S: [MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dot],
-    T: [MorseSignal.Dash],
-    U: [MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dash],
-    V: [MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dash],
-    W: [MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dash],
-    X: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dash],
-    Y: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dash],
-    Z: [MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dot],
-  };
-
-  //% fixedInstance
-  export let A: MorseCharacter = new MorseCharacter(_characters.A);
-  //% fixedInstance
-  export let B: MorseCharacter = new MorseCharacter(_characters.B);
-  //% fixedInstance
-  export let C: MorseCharacter = new MorseCharacter(_characters.C);
-  //% fixedInstance
-  export let D: MorseCharacter = new MorseCharacter(_characters.D);
-  //% fixedInstance
-  export let E: MorseCharacter = new MorseCharacter(_characters.E);
-  //% fixedInstance
-  export let F: MorseCharacter = new MorseCharacter(_characters.F);
-  //% fixedInstance
-  export let G: MorseCharacter = new MorseCharacter(_characters.G);
-  //% fixedInstance
-  export let H: MorseCharacter = new MorseCharacter(_characters.H);
-  //% fixedInstance
-  export let I: MorseCharacter = new MorseCharacter(_characters.I);
-  //% fixedInstance
-  export let J: MorseCharacter = new MorseCharacter(_characters.J);
-  //% fixedInstance
-  export let K: MorseCharacter = new MorseCharacter(_characters.K);
-  //% fixedInstance
-  export let L: MorseCharacter = new MorseCharacter(_characters.L);
-  //% fixedInstance
-  export let M: MorseCharacter = new MorseCharacter(_characters.M);
-  //% fixedInstance
-  export let N: MorseCharacter = new MorseCharacter(_characters.N);
-  //% fixedInstance
-  export let O: MorseCharacter = new MorseCharacter(_characters.O);
-  //% fixedInstance
-  export let P: MorseCharacter = new MorseCharacter(_characters.P);
-  //% fixedInstance
-  export let Q: MorseCharacter = new MorseCharacter(_characters.Q);
-  //% fixedInstance
-  export let R: MorseCharacter = new MorseCharacter(_characters.R);
-  //% fixedInstance
-  export let S: MorseCharacter = new MorseCharacter(_characters.S);
-  //% fixedInstance
-  export let T: MorseCharacter = new MorseCharacter(_characters.T);
-  //% fixedInstance
-  export let U: MorseCharacter = new MorseCharacter(_characters.U);
-  //% fixedInstance
-  export let V: MorseCharacter = new MorseCharacter(_characters.V);
-  //% fixedInstance
-  export let W: MorseCharacter = new MorseCharacter(_characters.W);
-  //% fixedInstance
-  export let X: MorseCharacter = new MorseCharacter(_characters.X);
-  //% fixedInstance
-  export let Y: MorseCharacter = new MorseCharacter(_characters.Y);
-  //% fixedInstance
-  export let Z: MorseCharacter = new MorseCharacter(_characters.Z);
 }
+
+//% color=#000080 icon="\uf086" block="Morse Code"
+//% groups=['Morse signal', 'Morse character']
+namespace morseCode {
+
+    /**
+    * Convert an array of Morse characters to an array of Morse Characters
+    */
+    //% color=#000080 block="convert %s to list of Morse signals"
+    //% s.shadow="MorseCharacterGetBlock"
+    //% blockSetVariable="Morse signal list"
+    //% group="Morse signal"
+    export function MorseCharToMorseSignalArray(s: MorseSignal[]): MorseSignal[] {
+        return s
+    }
+
+    const _characters = {
+        A: [MorseSignal.Dot, MorseSignal.Dash],
+        B: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dot],
+        C: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dot],
+        D: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dot],
+        E: [MorseSignal.Dot],
+        F: [MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dot],
+        G: [MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dot],
+        H: [MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dot],
+        I: [MorseSignal.Dot, MorseSignal.Dot],
+        J: [MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dash],
+        K: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dash],
+        L: [MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dot],
+        M: [MorseSignal.Dash, MorseSignal.Dash],
+        N: [MorseSignal.Dash, MorseSignal.Dot],
+        O: [MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dash],
+        P: [MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dot],
+        Q: [MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dash],
+        R: [MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dot],
+        S: [MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dot],
+        T: [MorseSignal.Dash],
+        U: [MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dash],
+        V: [MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dash],
+        W: [MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dash],
+        X: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dot, MorseSignal.Dash],
+        Y: [MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dash, MorseSignal.Dash],
+        Z: [MorseSignal.Dash, MorseSignal.Dash, MorseSignal.Dot, MorseSignal.Dot],
+    };
+
+    /**
+     * Check if signal is a dash or a dot
+     */
+    //% color=#000080 block="%s | is %compareto | ?"
+    //% group="Morse signal"
+    export function isDashOrDot(s: Number, compareto: MorseSignal): boolean {
+        if (s == compareto) {
+            return true
+        } else {
+            return false
+        }
+    }
+    /**
+     * Get a Morse character
+     */
+    //% blockId="MorseCharacterGetBlock" color=190 block="Morse %mchar"
+    //% group="Morse character"
+    export function _get(mchar: MorseCharacter): MorseSignal[] {
+        switch (mchar) {
+            case MorseCharacter.A:
+                return _characters.A;
+            case MorseCharacter.B:
+                return _characters.B;
+            case MorseCharacter.C:
+                return _characters.C;
+            case MorseCharacter.D:
+                return _characters.D;
+            case MorseCharacter.E:
+                return _characters.E;
+            case MorseCharacter.F:
+                return _characters.F;
+            case MorseCharacter.G:
+                return _characters.G;
+            case MorseCharacter.H:
+                return _characters.H;
+            case MorseCharacter.I:
+                return _characters.I;
+            case MorseCharacter.J:
+                return _characters.J;
+            case MorseCharacter.K:
+                return _characters.K;
+            case MorseCharacter.L:
+                return _characters.L;
+            case MorseCharacter.M:
+                return _characters.M;
+            case MorseCharacter.N:
+                return _characters.N;
+            case MorseCharacter.O:
+                return _characters.O;
+            case MorseCharacter.P:
+                return _characters.P;
+            case MorseCharacter.Q:
+                return _characters.Q;
+            case MorseCharacter.R:
+                return _characters.R;
+            case MorseCharacter.S:
+                return _characters.S;
+            case MorseCharacter.T:
+                return _characters.T;
+            case MorseCharacter.U:
+                return _characters.U;
+            case MorseCharacter.V:
+                return _characters.V;
+            case MorseCharacter.W:
+                return _characters.W;
+            case MorseCharacter.X:
+                return _characters.X;
+            case MorseCharacter.Y:
+                return _characters.Y;
+            case MorseCharacter.Z:
+                return _characters.Z;
+        }
+        return null;
+    }
+
+    /**
+     * Represents a Morse signal block
+     */
+    //% color=#000080 block="%signal"
+    //% group="Morse signal"
+    export function _signal(signal: MorseSignal): MorseSignal {
+        return signal;
+    }
+
+    /**
+     * Create a Morse word from an array of Morse Characters
+     */
+    //% color=190 block="%mchars"
+    //% mchars.shadow="lists_create_with"
+    //% mchars.defl="MorseCharacterGetBlock"
+    //% blockSetVariable="Morse char list"
+    export function createWordFromArrayOfChars(mchars: MorseSignal[][]): MorseSignal[][] {
+        let word: MorseSignal[][] = mchars
+        return word;
+    }
+
+    /**
+     * Convert a string to an array of Morse Characters
+     */
+    //% color=190 block="convert %s to list of Morse characters"
+    //% blockSetVariable="Morse char list"
+    export function stringToMorseArray(s: string): MorseSignal[][] {
+        let word: MorseSignal[][] = [];
+
+        const chars = s.split('');
+        chars.forEach(function (c, i) {
+            switch (c) {
+                case 'A':
+                case 'a':
+                    word.push(_characters.A);
+                    break;
+                case 'B':
+                case 'b':
+                    word.push(_characters.B);
+                    break;
+                case 'C':
+                case 'c':
+                    word.push(_characters.C);
+                    break;
+                case 'D':
+                case 'd':
+                    word.push(_characters.D);
+                    break;
+                case 'E':
+                case 'e':
+                    word.push(_characters.E);
+                    break;
+                case 'F':
+                case 'f':
+                    word.push(_characters.F);
+                    break;
+                case 'G':
+                case 'g':
+                    word.push(_characters.G);
+                    break;
+                case 'H':
+                case 'h':
+                    word.push(_characters.H);
+                    break;
+                case 'I':
+                case 'i':
+                    word.push(_characters.I);
+                    break;
+                case 'J':
+                case 'j':
+                    word.push(_characters.J);
+                    break;
+                case 'K':
+                case 'k':
+                    word.push(_characters.K);
+                    break;
+                case 'L':
+                case 'l':
+                    word.push(_characters.L);
+                    break;
+                case 'M':
+                case 'm':
+                    word.push(_characters.M);
+                    break;
+                case 'N':
+                case 'n':
+                    word.push(_characters.N);
+                    break;
+                case 'O':
+                case 'o':
+                    word.push(_characters.O);
+                    break;
+                case 'P':
+                case 'p':
+                    word.push(_characters.P);
+                    break;
+                case 'Q':
+                case 'q':
+                    word.push(_characters.Q);
+                    break;
+                case 'R':
+                case 'r':
+                    word.push(_characters.R);
+                    break;
+                case 'S':
+                case 's':
+                    word.push(_characters.S);
+                    break;
+                case 'T':
+                case 't':
+                    word.push(_characters.T);
+                    break;
+                case 'U':
+                case 'u':
+                    word.push(_characters.U);
+                    break;
+                case 'V':
+                case 'v':
+                    word.push(_characters.V);
+                    break;
+                case 'W':
+                case 'w':
+                    word.push(_characters.W);
+                    break;
+                case 'X':
+                case 'x':
+                    word.push(_characters.X);
+                    break;
+                case 'Y':
+                case 'y':
+                    word.push(_characters.Y);
+                    break;
+                case 'Z':
+                case 'z':
+                    word.push(_characters.Z);
+                    break;
+            }
+        });
+        return word;
+    }
+}
+
